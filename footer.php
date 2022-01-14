66eebb5d14ea4e08a8da1fa901f464e2f3566b9d
francois-niang1 <francois.niang@laplateforme.io>
2022-01-14 12:18:48 +0100

    arrangement footer

--- footer.php
+++ footer.php
@@ -1,22 +1,53 @@
 <link href="css/footer.css" rel="stylesheet">
 <footer>
-    <div class="footer">
-        <div class="footer1">
-        Accueil<a href="index.php"><img class="socialMedia" alt="Accueil" src="https://cdn-icons-png.flaticon.com/512/1239/1239292.png"></a>
-        Planning<a href="planning.php"><img class="socialMedia" alt="Planning" src="http://cdn.onlinewebfonts.com/svg/img_351420.png"></a>
-        <?php if(!isset($_SESSION['user'])){ echo 'Connexion<a href="connexion.php"><img class="socialMedia" alt="Connexion" src="http://cdn.onlinewebfonts.com/svg/img_201469.png"></a>';} ?>
-        <?php if(!isset($_SESSION['user'])){ echo 'Inscription<a href="inscription.php"><img class="socialMedia" alt="Inscription" src="https://www.vbvb.fr/wp-content/uploads/2016/04/icone_sinscrire.png"></a>';} ?>
-        <?php if(isset($_SESSION['user'])){ echo 'Profil<a href="profil.php"><img class="socialMedia" alt="Profil" src="http://cdn.onlinewebfonts.com/svg/img_311846.png"></a>';} ?>
+    <div class="container">
+        <div class="part-up">
+            <section class="about">
+                <h2>About</h2>
+                <p>
+                    Une envie d'un endroit où faire la fête, où travailler dans le calme, où organiser vos réunion ?
+                    Eb bien vous topmber à pic !! Venez réserver une salle sur notre site pour profiter d'un espace
+                    calme et conviviale pour votre plus grand bonheur. Pour réserver rien de plus simple connectez vous pour
+                    réserver une salle, vous n,'avez pas de compte ? ce n'est pas grave inscrivez vous et par la suite vous pourrez
+                    reserver votre salle. Choisissez la date, l'heure, donner un titre et une description à votre créneau et une 
+                    salle vous sera decerné.
+                </p>
+            </section>
+            <section class="category">
+                <h2>Catégories</h2>
+                <ul>
+                    <li> <a href="index.php"> Accueil </a></li>
+                    <li> <a href="planning.php"> Planning </a></li>
+                    <li> <a href="#link-foot"> Drill </a></li>
+                    <li> <a href="#link-foot"> RnB </a></li>
+                </ul>
+            </section>
+            <section class="link">
+                <h2>Liens</h2>
+                <ul>
+                    <li> <a href="reservation-form.php"> Reserver une salle </a></li>
+                    <?php
+                    if(!isset($_SESSION['user'])){
+                    echo '
+                    <li> <a href="connexion.php"> Connexion </a></li>
+                    <li> <a href="inscription.php"> Inscription </a></li>';
+                    }
+                    ?>
+                    <li> <a href="profil.php"> Profil </a></li>
+                </ul>
+            </section>
         </div>
-        <div class="footer2">
-            <h2>Copyright © 2022 Hugo, François & Remi. All Rights Reserved</h2>
-        </div>
-        <div class="footer3">
-            <a href="https://twitter.com/"><img class="socialMedia" alt="Twitter" src="images/Twitter.png"></a>
-            <a href="https://facebook.com/"><img class="socialMedia" alt="Facebook" src="images/Facebook.png"></a>
-            <a href="https://instagram.com/"><img class="socialMedia" alt="Instagram" src="images/Instagram.png"></a>
-            <a href="https://youtube.com/"><img class="socialMedia" alt="Youtube" src="images/Youtube.png"></a>
-            <a href="https://github.com/hugo-chabert/reservation-salles"><img class="socialMedia2" alt="GitHub" src="images/GitHub-Logo.png"></a>
+        <hr>
+        <div class="part-down">
+            <section class="copy">
+                <p class="copyright-text">Copyright &copy; All Rights Reserved by <a href="#">Francois Niang</a>, <a href="#">Hugo Chabert</a></p>
+            </section>
+            <section class="social">
+                <li><button class="facebook"><a  href="#"><img src="https://img.icons8.com/ios/30/000000/facebook--v1.png" alt="social" width="30px"></a></button></li>
+                <li><button class="twitter"><a  href="#"><img src="https://img.icons8.com/ios/30/000000/twitter--v1.png" alt="social"width="30px" ></a></button></li>
+                <li><button class="instagram"><a  href="#"><img src="https://img.icons8.com/ios/30/000000/instagram-new--v1.png" alt="social"width="30px"></a></button></li>
+                <li><button class="github"><a  href="#"><img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" alt="social"width="30px"></a></button></li>
+            </section>
         </div>
     </div>
 </footer>
