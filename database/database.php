60557a405383aa8b25602a8364c8f38de6ab3ec6
Hugo <hugo.chabert@laplateforme.io>
2022-01-28 14:18:32 +0100

    Ajout identifiants db pour plesk

--- database/database.php
+++ database/database.php
@@ -10,7 +10,7 @@ class Database
     public static function connect_db(): PDO
     {
         try {
-            $bdd = new PDO("mysql:host=localhost;dbname=reservationsalles;charset=utf8", "root", "root");
+            $bdd = new PDO("mysql:host=localhost;dbname=hugo-chabert_reservationsalles;charset=utf8", "hugo-chabertRS", "Chabert13");
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if (!$bdd) {
                 die("Connexion a la bdd impossible");
