a61908c9e7d5439708f93edf7c5ae83fcfe9b2af
Hugo <hugo.chabert@laplateforme.io>
2022-01-28 14:51:05 +0100

    Resolution pb connection db

--- database/database.php
+++ database/database.php
@@ -10,7 +10,7 @@ class Database
     public static function connect_db(): PDO
     {
         try {
-            $bdd = new PDO("mysql:host=localhost:3306;dbname=hugo-chabert_reservationsalles;charset=utf8", "hugo-chabertRS", "Chabert13");
+            $bdd = new PDO('mysql:host=localhost;dbname=hugo-chabert_reservationsalles;charset=utf8', 'hugo-chabertRS', 'Chabert13');
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if (!$bdd) {
                 die("Connexion a la bdd impossible");
