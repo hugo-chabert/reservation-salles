bc21106c9052114f21edf07ad532ba3f1ae6b98a
Hugo <hugo.chabert@laplateforme.io>
2022-02-15 16:19:15 +0100

    Modification mdp bdd

--- database/database.php
+++ database/database.php
@@ -9,7 +9,7 @@ class Database
     public static function connect_db(): PDO
     {
         try {
-            $bdd = new PDO('mysql:host=localhost;dbname=hugo-chabert_reservationsalles;charset=utf8', 'hugo-chabertRS', 'Chabert13');
+            $bdd = new PDO('mysql:host=localhost;dbname=hugo-chabert_reservationsalles;charset=utf8', 'hugo-chabertRS', 'chabertRS');
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             if (!$bdd) {
                 die("Connexion a la bdd impossible");
